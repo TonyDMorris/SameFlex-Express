6ffc9cfd026f65c7fd86a2b5dcaa4814d48c0b1c
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-23 16:09:26 +0100

    added param for id search in articles

--- controllers/get-articles.js
+++ controllers/get-articles.js
@@ -1,7 +1,9 @@
 const fetchArticles = require("../models/fetch-articles");
 const getArticles = (req, res, next) => {
   const { sort_by, order, author, topic } = req.query;
-  return fetchArticles(sort_by, order, author, topic)
+  const { article_id } = req.params;
+
+  return fetchArticles(sort_by, order, author, topic, article_id)
     .then(articles => {
       res.status(200).send({ articles });
     })
