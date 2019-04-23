9cff9eef3d6877ba721b2b5b12eaa9c87ba9ae75
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-23 16:10:54 +0100

    added routes for patch and get/:article_id

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,7 @@
 const articlesRouter = require("express").Router();
 const getArticles = require("../controllers/get-articles");
-
+const patchVotes = require("../controllers/patchVotes");
 articlesRouter.get("/", getArticles);
+articlesRouter.get("/:article_id", getArticles);
+articlesRouter.patch("/:article_id", patchVotes);
 module.exports = articlesRouter;
