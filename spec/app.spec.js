8b58fdd2e056a2db6414ed36b9e27ad7f9e4284f
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-23 13:10:50 +0100

    creating tests for optional quries

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,13 +1,14 @@
 process.env.NODE_ENV = "test";
 
 const { expect } = require("chai");
-
+const chai = require("chai");
+chai.use(require("chai-sorted"));
 const app = require("../app");
 const knex = require("../db/connection");
 
 const request = require("supertest");
 
-describe("/", () => {
+describe.only("/", () => {
   beforeEach(() => knex.seed.run());
   after(() => knex.destroy());
 
@@ -20,52 +21,119 @@ describe("/", () => {
           expect(body.ok).to.equal(true);
         });
     });
+    describe("/topics", () => {
+      it("GET should return a status code of 200 and an array  of topic objects proceeded by a topics key", () => {
+        return request(app)
+          .get("/api/topics")
+          .expect(200)
+          .then(data => {
+            const { topics } = data.body;
+            expect(topics).to.be.an("array");
+            expect(topics[0]).to.have.keys("slug", "description");
+          });
+      });
+      it("POST should return a 405 method not allowed error", () => {
+        return request(app)
+          .post("/api/topics")
+          .expect(405)
+          .then(err => {
+            const { msg } = err.body;
+            expect(msg).to.eql("Method Not Allowed");
+          });
+      });
+      it("PUT should return a 405 method not allowed error", () => {
+        return request(app)
+          .put("/api/topics")
+          .expect(405)
+          .then(err => {
+            const { msg } = err.body;
+            expect(msg).to.eql("Method Not Allowed");
+          });
+      });
+      it("PATCH should all return a 405 method not allowed error", () => {
+        return request(app)
+          .patch("/api/topics")
+          .expect(405)
+          .then(err => {
+            const { msg } = err.body;
+            expect(msg).to.eql("Method Not Allowed");
+          });
+      });
+      it("DELETE should all return a 405 method not allowed error", () => {
+        return request(app)
+          .delete("/api/topics")
+          .expect(405)
+          .then(err => {
+            const { msg } = err.body;
+            expect(msg).to.eql("Method Not Allowed");
+          });
+      });
+    });
   });
-  describe("/topics", () => {
-    it("GET should return a status code of 200 and an array  of topic objects proceeded by a topics key", () => {
+  describe("/articles", () => {
+    it("GET should return a status code of 200 and an array of articles proceeded by an articles key", () => {
       return request(app)
-        .get("/api/topics")
+        .get("/api/articles")
         .expect(200)
-        .then(data => {
-          const { topics } = data.body;
-          expect(topics).to.be.an("array");
-          expect(topics[0]).to.have.keys("slug", "description");
+        .then(({ body }) => {
+          const { articles } = body.articles;
+          expect(articles[0]).to.have.keys(
+            "article_id",
+            "title",
+            "body",
+            "votes",
+            "topic",
+            "author",
+            "created_at",
+            "comment_count"
+          );
+
+          expect(articles).to.be.an("array");
+          expect(articles[0].article_id).to.be.a("number");
+          expect(articles[0].title).to.be.a("string");
+          expect(articles[0].body).to.be.a("string");
+          expect(articles[0].topic).to.be.a("string");
+          expect(articles[0].author).to.be.a("string");
+          expect(articles[0].votes).to.be.a("number");
         });
     });
-    it("POST,PUT,PATCH,AND DELETE should all return a 405 method not allowed error", () => {
+    it("should be sorted by author by default", () => {
       return request(app)
-        .post("/api/topics")
-        .expect(405)
-        .then(err => {
-          const { msg } = err.body;
-          expect(msg).to.eql("Method Not Allowed");
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body }) => {
+          const { articles } = body.articles;
+          expect(articles).to.be.descendingBy("author");
         });
     });
-    it("POST,PUT,PATCH,AND DELETE should all return a 405 method not allowed error", () => {
+    it("accept a custom query to sort by ", () => {
       return request(app)
-        .put("/api/topics")
-        .expect(405)
-        .then(err => {
-          const { msg } = err.body;
-          expect(msg).to.eql("Method Not Allowed");
+        .get("/api/articles?sort_by=votes")
+        .expect(200)
+        .then(({ body }) => {
+          const { articles } = body.articles;
+          console.log(articles);
+          expect(articles).to.be.descendingBy("votes");
         });
     });
-    it("POST,PUT,PATCH,AND DELETE should all return a 405 method not allowed error", () => {
+    it("accepts an optional order query to for order direction of sort by", () => {
       return request(app)
-        .patch("/api/topics")
-        .expect(405)
-        .then(err => {
-          const { msg } = err.body;
-          expect(msg).to.eql("Method Not Allowed");
+        .get("/api/articles?sort_by=votes&order=asc")
+        .expect(200)
+        .then(({ body }) => {
+          const { articles } = body.articles;
+          console.log(articles);
+          expect(articles).to.be.ascendingBy("votes");
         });
     });
-    it("POST,PUT,PATCH,AND DELETE should all return a 405 method not allowed error", () => {
+    it("accept additional filter paramaters", () => {
       return request(app)
-        .delete("/api/topics")
-        .expect(405)
-        .then(err => {
-          const { msg } = err.body;
-          expect(msg).to.eql("Method Not Allowed");
+        .get("/api/articles?author=mitch")
+        .expect(200)
+        .then(({ body }) => {
+          const { articles } = body.articles;
+          console.log(articles);
+          expect(articles[0].author).to.eql("mitch");
         });
     });
   });
