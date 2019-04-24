013ad388d06b08148c1874743ef2d589c4b6203e
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-24 16:22:42 +0100

    commit new url var

--- knexfile.js
+++ knexfile.js
@@ -13,8 +13,7 @@ const baseConfig = {
 const customConfigs = {
   development: {
     connection: {
-      database:
-        "postgres://pcfnwnrmxikeej:620cd79399f0cc8a43afdc5bd0a483138de8f4a2820994cc6c792887fce4c5df@ec2-54-197-239-115.compute-1.amazonaws.com:5432/d956dde9850oph",
+      database: process.env.DATABASE_URL || "nc_news",
       username: "tony",
       password: "password"
     }
