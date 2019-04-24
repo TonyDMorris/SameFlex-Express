31c7212753a489817226578cab0a442aea429c39
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-24 13:47:19 +0100

    added method error handling to comments router

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,10 @@
 const commentsRouter = require("express").Router();
 const patchCommentVotes = require("../controllers/patch-comment-votes");
 const deleteComment = require("../controllers/delete-comment");
+const { methodNotAllowed } = require("../errors/index");
 commentsRouter
   .route("/:comment_id")
   .patch(patchCommentVotes)
-  .delete(deleteComment);
+  .delete(deleteComment)
+  .all(methodNotAllowed);
 module.exports = commentsRouter;
