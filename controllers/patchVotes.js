48e471b5b2c10e23a19f5ff06368f52ea1826681
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-24 11:26:30 +0100

    moved error handling from votes model to cntrler

--- controllers/patchVotes.js
+++ controllers/patchVotes.js
@@ -2,7 +2,14 @@ const incrementVotes = require("../models/increment-votes");
 const patchVotes = (req, res, next) => {
   const { inc_votes } = req.body;
   const { article_id } = req.params;
-
+  if (!inc_votes || !article_id) {
+    const err = {
+      msg:
+        "The request could not be understood by the server due to malformed syntax. The client SHOULD NOT repeat the request without modifications.",
+      status: 400
+    };
+    return next(err);
+  }
   incrementVotes(inc_votes, +article_id)
     .then(votes => {
       res.status(201).send(votes);
--- models/increment-votes.js
+++ models/increment-votes.js
@@ -1,13 +1,5 @@
 const knex = require("../db/connection");
 const incrementVotes = (inc_votes, article_id) => {
-  if (!inc_votes || !article_id) {
-    const err = {
-      msg:
-        "The request could not be understood by the server due to malformed syntax. The client SHOULD NOT repeat the request without modifications.",
-      status: 400
-    };
-    return Promise.reject(err);
-  }
   return knex("articles")
     .where({ article_id })
     .increment("votes", inc_votes)
