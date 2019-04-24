813907b02e499fe09420f386d47f7a93a52e7612
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-24 16:13:56 +0100

    adding heroku db key

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,8 @@ const baseConfig = {
 const customConfigs = {
   development: {
     connection: {
-      database: "nc_news",
+      database:
+        "postgres://pcfnwnrmxikeej:620cd79399f0cc8a43afdc5bd0a483138de8f4a2820994cc6c792887fce4c5df@ec2-54-197-239-115.compute-1.amazonaws.com:5432/d956dde9850oph",
       username: "tony",
       password: "password"
     }
