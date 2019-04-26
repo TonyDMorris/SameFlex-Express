33c757984169846f695c5dc9b41cabb37818c6d4
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-26 10:17:52 +0100

    removed console.log

--- models/increment-comment-votes.js
+++ models/increment-comment-votes.js
@@ -1,6 +1,5 @@
 const knex = require("../db/connection");
 const incrementCommentVotes = (inc_votes = 0, comment_id) => {
-  console.log(inc_votes);
   return knex("comments")
     .where({ comment_id })
     .increment("votes", +inc_votes)
