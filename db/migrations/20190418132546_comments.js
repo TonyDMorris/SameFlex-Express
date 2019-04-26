e16af4e38abe6c84b5144a55dc98fa93a24f32cf
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-26 10:17:06 +0100

    added cascade to comments table for article del

--- db/migrations/20190418132546_comments.js
+++ db/migrations/20190418132546_comments.js
@@ -4,7 +4,10 @@ exports.up = function(knex, Promise) {
     commentsTable.string("author").notNull();
     commentsTable.foreign("author").references("users.username");
     commentsTable.integer("article_id").notNull();
-    commentsTable.foreign("article_id").references("articles.article_id");
+    commentsTable
+      .foreign("article_id")
+      .references("articles.article_id")
+      .onDelete("cascade");
     commentsTable.integer("votes").defaultTo(0);
     commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
     commentsTable.text("body", "longtext").notNull();
