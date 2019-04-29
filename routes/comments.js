a0c0bd5b276cb7845fe5afb04a9b59207fc4754b
TonyDMorris <anthonymorris86@googlemail.com>
2019-04-29 14:39:34 +0100

    adding auth

--- routes/comments.js
+++ routes/comments.js
@@ -2,10 +2,11 @@ const commentsRouter = require("express").Router();
 const patchCommentVotes = require("../controllers/patch-comment-votes");
 const deleteComment = require("../controllers/delete-comment");
 const { methodNotAllowed } = require("../errors/index");
+const authToken = require("../controllers/auth-token");
 commentsRouter
   .route("/:comment_id")
-  .patch(patchCommentVotes)
-  .delete(deleteComment)
+  .patch(authToken, patchCommentVotes)
+  .delete(authToken, deleteComment)
   .all(methodNotAllowed);
 commentsRouter.all("/", methodNotAllowed);
 module.exports = commentsRouter;
