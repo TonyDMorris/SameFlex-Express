5600ab9438472c3a6a38bc0ee94c77aa27fe9e0e
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-18 10:53:36 +0100

    added a count key to returning articles

--- models/fetch-articles.js
+++ models/fetch-articles.js
@@ -8,43 +8,57 @@ const fetchArticles = (
   limit = 10,
   page = 1
 ) => {
-  return knex
-    .select(
-      "articles.article_id",
-      "articles.title",
-      "articles.body",
-      "articles.votes",
-      "articles.topic",
-      "articles.author",
-      "articles.created_at"
-    )
-    .sum("articles.article_id as article_count")
-    .count("comments.article_id as comment_count")
-    .from("articles")
-    .groupBy("articles.article_id")
-    .leftJoin("comments", "articles.article_id", "comments.article_id")
-    .modify(queryBuilder => {
-      if (author) {
-        queryBuilder.where({ "articles.author": author });
-      }
-      if (topic) {
-        queryBuilder.where({ "articles.topic": topic });
-      }
-      if (id) {
-        queryBuilder.where({ "articles.article_id": id });
-      }
-    })
-    .limit(limit)
-    .offset(page * limit - limit)
-    .orderBy(sort_by, order)
-    .then(articles => {
-      if (articles.length === 1) {
-        const [article] = articles;
-        return { article };
-      } else {
-        return { articles };
-      }
-    });
+  return Promise.all([
+    knex
+      .select(
+        "articles.article_id",
+        "articles.title",
+        "articles.body",
+        "articles.votes",
+        "articles.topic",
+        "articles.author",
+        "articles.created_at"
+      )
+      .count("comments.article_id as comment_count")
+      .from("articles")
+      .groupBy("articles.article_id")
+      .leftJoin("comments", "articles.article_id", "comments.article_id")
+      .modify(queryBuilder => {
+        if (author) {
+          queryBuilder.where({ "articles.author": author });
+        }
+        if (topic) {
+          queryBuilder.where({ "articles.topic": topic });
+        }
+        if (id) {
+          queryBuilder.where({ "articles.article_id": id });
+        }
+      })
+      .limit(limit)
+      .offset(page * limit - limit)
+      .orderBy(sort_by, order),
+    knex("articles")
+      .count("article_id")
+      .modify(queryBuilder => {
+        if (author) {
+          queryBuilder.where({ "articles.author": author });
+        }
+        if (topic) {
+          queryBuilder.where({ "articles.topic": topic });
+        }
+        if (id) {
+          queryBuilder.where({ "articles.article_id": id });
+        }
+      })
+  ]).then(([articles, [{ count }]]) => {
+    console.log(count);
+    if (articles.length === 1) {
+      const [article] = articles;
+      return { article, article_count: count };
+    } else {
+      return { articles, article_count: count };
+    }
+  });
 };
 
 module.exports = fetchArticles;
